e73e2e94f929f584c85fc7a124cdfa92f431b45d
jinges <jinges2012@gmail.com>
2019-11-24 23:30:57 +0800

    add css

--- src/components/RightBoxUserInfo.vue
+++ src/components/RightBoxUserInfo.vue
@@ -180,33 +180,30 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-	.user_info {
-		display: flex;
-		flex-direction: column;
-		background: rgba(0, 0, 0, 0.6);
-		color: #fff;
-		min-height:100%;
-		padding-bottom: 12px;
-
-		.user_info_item{
-			padding: 6px 12px;
-
-			.user_info_title{
-				text-align: center;
-				font-size: 16px;
-				line-height: 2;
-			}
-			.user_info_wrap{
-				border-radius: 4px;
-				background-color: #333;
-				background-color:rgba(255,255,255,.1);
-				padding: 12px 0;
-				min-height: 80px;
-
+.user_info {
+  color: #fff;
+  padding-bottom: 12px;
+  height: 1px;
+  -webkit-box-flex: 1;
+  -ms-flex-positive: 1;
+  flex-grow: 1;
+  overflow: auto;
+  .user_info_item {
+    padding: 6px 12px;
+    .user_info_title {
+      text-align: center;
+      font-size: 16px;
+      line-height: 2;
+    }
+    .user_info_wrap {
+      border-radius: 4px;
+      background-color: rgba(0, 0, 0, 0.2);
+      padding: .6em 1em;
+      min-height: 80px;
+			font-size: .9em;
       .user_info_wrap_li {
         position: relative;
         padding: 0.2em;
-        min-height: 20px;
         .user_info_wrap_sub {
           color: #aaa;
           width: 4em;
@@ -214,25 +211,72 @@ export default {
           text-align: justify;
           text-align-last: justify;
         }
-
-					.user_info_wrap_text{
-						margin-left: 6.5em;
-						margin-right: .5em;
-						/*overflow: hidden;
-						text-overflow:ellipsis;
-						white-space: nowrap;*/
-						display: block;
-					}
+        .user_info_wrap_text {
+          margin-left: 5.5em;
+          margin-right: 0.5em;
+          display: block;
+          padding: 0.1em;
+          &.editor {
+            border: 1px solid rgba(255, 255, 255, 0.2);
+            background: rgba(255, 255, 255, 0.14);
+            border-radius: 4px;
+            padding: 0.04em 0.6em;
+            &:focus {
+              outline: none;
+            }
+          }
+        }
+      }
+      .user_info_wrap_tag {
+        display: inline-block;
+        border-radius: 4px;
+        border: 1px solid #666;
+        padding: 0 0.5em;
+        margin: 0.5em 0.3em;
+        line-height: 2;
+        position: relative;
+				font-size: .88em;
+        cursor: pointer;
+        .del {
+          width: 12px;
+          height: 12px;
+          display: block;
+          border-radius: 50%;
+          position: absolute;
+          right: -6px;
+          top: -6px;
+          background: #fff;
+          color: #000;
+          font-size: 13px;
+          text-align: center;
+          cursor: pointer;
+        }
+				&.no_sel{
+					color: #5297e8;
+					border-color: #5297e8;
 				}
-				.user_info_wrap_tag{
-					display: inline-block;
-					border-radius: 4px;
-					border: 1px solid #666;
-					padding: 0 .5em;
-					margin: .5em .3em;
-					line-height: 2;
-				}
+      }
+			.tag_tip{
+				margin: .1em;
 			}
-		}
-	}
+    }
+  }
+}
+.user_content {
+  height: 100%;
+  overflow: auto;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  display: flex;
+  flex-direction: column;
+  display: -webkit-box;
+  position: relative;
+  overflow: hidden;
+}
+.user_bottom {
+  padding: 10px;
+  text-align: center;
+}
 </style>
