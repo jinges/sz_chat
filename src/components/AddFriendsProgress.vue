1de51c1c8e01be3a12a3daef6842ae9c52e0ebe8
jinges <jinges2012@gmail.com>
2019-11-25 00:37:20 +0800

    merge

--- src/components/AddFriendsProgress.vue
+++ src/components/AddFriendsProgress.vue
@@ -4,51 +4,88 @@
 			<div class="title">当前进度</div>
 			<div class="progress">
 				<div class="name">总进度</div>
-				<div class="value">50/100</div>
+				<div class="value">{{addFriendsSuccessCount}}/{{addFriendsAllCount}}</div>
 			</div>
 			<div class="progress-info">
 				<div class="name">等待添加</div>
-				<div class="value">50</div>
+				<div class="value">{{addFriendsWaitCount}}</div>
 			</div>
 			<div class="progress-info">
 				<div class="name">添加成功</div>
-				<div class="value">50</div>
+				<div class="value">{{addFriendsSuccessCount}}</div>
 			</div>
 			<div class="progress-info">
 				<div class="name">被拒绝</div>
-				<div class="value">50</div>
+				<div class="value">{{addFriendsRefuseCount}}</div>
 			</div>
 			<div class="progress-info">
 				<div class="name">未通过</div>
-				<div class="value">50</div>
+				<div class="value">{{addFriendsUnansweredCount}}</div>
 			</div>
 			<div class="progress-info">
 				<div class="name">查找失败</div>
-				<div class="value">50</div>
+				<div class="value">{{addFriendsFailCount}}</div>
 			</div>
 			<div class="progress-info">
 				<div class="name">重复好友</div>
-				<div class="value">50</div>
+				<div class="value">{{addFriendsRepeatCount}}</div>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
+	import $ from 'jquery'
+	import util from '@/util/util.js'
 	export default {
 		data() {
 			return {
-				
+				addFriendsAllCount: 0,
+				addFriendsWaitCount: 0,
+				addFriendsSuccessCount: 0,
+				addFriendsRefuseCount: 0,
+				addFriendsUnansweredCount: 0,
+				addFriendsFailCount: 0,
+				addFriendsRepeatCount: 0
 			}
 		},
-		mounted(){
-
+		mounted() {
+			this.$axios.post('/queryAddfriendByPage', {
+				imei: util.getImei(),
+				my_wxid: util.getMyWxId()
+			}).then(data => {
+				this.addFriendsAllCount = data.data.length;
+				for (var i = 0; i < data.data.length; i++) {
+					data.data[i].createdAt = data.data[i].createdAt.replace('T', ' ');
+					switch (data.data[i].status) {
+						case '添加成功':
+							this.addFriendsSuccessCount++;
+							break;
+						case '对方已拒绝':
+							this.addFriendsRefuseCount++;
+							break;
+						case '未回应':
+							this.addFriendsUnansweredCount++;
+							break;
+						case '查找失败':
+							this.addFriendsFailCount++;
+							break;
+						case '好友已经存在':
+							this.addFriendsRepeatCount++;
+							break;
+						case '需好友验证':
+							this.addFriendsWaitCount ++;
+							break;
+					}
+				}
+				this.addFriendsInfo = data;
+			}).catch(() => {});
 		},
 		methods: {
-			
+
 		},
 		created() {
-			
+
 		}
 	}
 </script>
@@ -59,7 +96,7 @@
 		flex-direction: column;
 		background: rgba(0, 0, 0, 0.6);
 		color: #fff;
-		
+
 		.addFriendsProgress {
 			.title {
 				text-align: center;
@@ -69,19 +106,19 @@
 				line-height: 50px;
 			}
 		}
-		
+
 		.progress {
 			display: flex;
 			height: 60px;
 			border-bottom: 1px solid #5a5a5a;
 			position: relative;
-			
+
 			.name {
 				left: 15%;
 				position: absolute;
 				bottom: 10px;
 			}
-			
+
 			.value {
 				color: #aaa;
 				right: 15%;
@@ -89,18 +126,18 @@
 				bottom: 10px;
 			}
 		}
-		
+
 		.progress-info {
 			display: flex;
 			height: 40px;
 			position: relative;
-			
+
 			.name {
 				left: 15%;
 				position: absolute;
 				bottom: 10px;
 			}
-			
+
 			.value {
 				color: #aaa;
 				right: 15%;
