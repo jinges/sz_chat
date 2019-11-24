2b38840d368d20e21517d857f14e8d6f6e69f118
jinges <jinges2012@gmail.com>
2019-11-24 21:31:12 +0800

    群发消息

--- src/components/RightBoxUserInfo.vue
+++ src/components/RightBoxUserInfo.vue
@@ -1,192 +1,272 @@
 <template>
-	<div class="user_info">
-		<div class="user_info_item" v-for="(item,index) in userInfoData">
-			<div class="user_info_title">
-				{{item.title}}
-			</div>
-			<div class="user_info_wrap">
-				<div class="user_info_wrap_li" v-for="(item,index) in item.content">
-					<span class="user_info_wrap_sub">{{item.sub}}</span>
-					<span class="user_info_wrap_text">{{item.text}}</span>
-				</div>
-
-			</div>
-		</div>
-
-		<div class="user_info_item">
-			<div class="user_info_title">
-				标签
-			</div>
-			<div class="user_info_wrap">
-				<span class="user_info_wrap_tag" v-for="(item,index) in tagData" :key="item.index">{{item.tagName}}</span>
-			</div>
-		</div>
-
-	</div>
+  <div class="user_content">
+    <div class="user_info">
+      <div class="user_info_item" v-for="(item,index) in userInfoData" :key="index">
+        <div class="user_info_title">{{item.title}}</div>
+        <div class="user_info_wrap">
+          <div class="user_info_wrap_li" v-for="(item,uindex) in item.content" :key="uindex">
+            <span class="user_info_wrap_sub">{{item.sub}}</span>
+            <span
+              class="user_info_wrap_text"
+              :class="{'editor': (editState && index == 1)}"
+              :contenteditable="(editState && index == 1)"
+            >{{item.text}}</span>
+          </div>
+        </div>
+      </div>
+      <div class="user_info_item">
+        <div class="user_info_title">标签</div>
+        <div class="user_info_wrap">
+          <p class="tag_tip" v-show="editState">已添加</p>
+          <span class="user_info_wrap_tag" v-for="(item,index) in tagData" :key="index">
+            {{item.tagName}}
+            <em class="del el-icon-close" @click="removeTag(index)" v-show="editState"></em>
+          </span>
+          <div v-show="editState">
+            <p class="tag_tip">未添加</p>
+            <span class="user_info_wrap_tag no_sel" @click="checkTag(index)" v-for="(item,index) in allTags" :key="index">
+              {{item.tagName}}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="user_bottom">
+      <el-button type="primary" @click="editor()" size="mini">{{editState?'保存':'修改'}}</el-button>
+    </div>
+  </div>
 </template>
 
 <script>
-	import util from '@/util/util.js'
+import util from "@/util/util.js";
 
-	export default {
-		name: 'RightBoxUserInfo',
-		data() {
-			return {
-				title: '',
-				loading: false,
-				userInfoData: [
-					{
-						title:"微信资料",
-						content:[{
-							sub:"备注",
-							text:"有意向客户有意向客户有意向客户有意向客户有意向客户"
-						},{
-							sub:"地区",
-							text:"广东 深圳"
-						},{
-							sub:"微信号",
-							text:"A1234566"
-						},{
-							sub:"来源",
-							text:"通过手机号添加"
-						}]
-					},{
-						title:"客户档案",
-						content:[{
-							sub:"真实姓名",
-							text:"张三"
-						},{
-							sub:"性别",
-							text:"男"
-						},{
-							sub:"偏好",
-							text:"高端车型"
-						},{
-							sub:"学历",
-							text:"本科"
-						}]
-					}
-				],
-				tagData:[
-					{
-						tagName:"意向客户",
-						tagId:"123456"
-					},
-					{
-						tagName:"高端客户",
-						tagId:"456789"
-					},
-					{
-						tagName:"高端客户",
-						tagId:"456789"
-					},
-					{
-						tagName:"高端客户",
-						tagId:"456789"
-					},
-					{
-						tagName:"高端客户高端客户高端客户",
-						tagId:"456789"
-					},
-					{
-						tagName:"高端客户",
-						tagId:"456789"
-					},
-					{
-						tagName:"高端客户高端",
-						tagId:"456789"
-					},
-					{
-						tagName:"高端客户",
-						tagId:"456789"
-					},
-					{
-						tagName:"高端客户",
-						tagId:"456789"
-					},
-					{
-						tagName:"高端客户",
-						tagId:"456789"
-					},
-					{
-						tagName:"高端客户",
-						tagId:"456789"
-					},
-					{
-						tagName:"高端客户",
-						tagId:"456789"
-					}
-				]
+export default {
+  name: "RightBoxUserInfo",
+  data() {
+    return {
+      title: "",
+      loading: false,
+      editState: false,
+      userInfoData: [
+        {
+          title: "微信资料",
+          content: [
+            {
+              sub: "备注",
+              text: "有意向客户有意向客户有意向客户有意向客户有意向客户"
+            },
+            {
+              sub: "地区",
+              text: "广东 深圳"
+            },
+            {
+              sub: "微信号",
+              text: "A1234566"
+            },
+            {
+              sub: "来源",
+              text: "通过手机号添加"
+            }
+          ]
+        },
+        {
+          title: "客户档案",
+          content: [
+            {
+              sub: "真实姓名",
+              text: "张三"
+            },
+            {
+              sub: "性别",
+              text: "男"
+            },
+            {
+              sub: "偏好",
+              text: "高端车型"
+            },
+            {
+              sub: "学历",
+              text: "本科"
+            }
+          ]
+        }
+      ],
+      tagData: [
+        {
+          tagName: "意向客户",
+          tagId: "123456"
+        },
+        {
+          tagName: "高端客户",
+          tagId: "456789"
+        },
+        {
+          tagName: "高端客户",
+          tagId: "456789"
+        },
+        {
+          tagName: "高端客户",
+          tagId: "456789"
+        },
+        {
+          tagName: "高端客户高端客户高端客户",
+          tagId: "456789"
+        }
+			],
+			allTags:[
+        {
+          tagName: "意向客户",
+          tagId: "123456"
+        },
+        {
+          tagName: "高端客户",
+          tagId: "456789"
+        },
+        {
+          tagName: "高端客户",
+          tagId: "456789"
+        },
+			]
+    };
+  },
+  watch: {},
+  components: {},
+  methods: {
+    editor() {
+			if(this.editState) {
+				console.log(this.userInfoData)
 			}
+      this.editState = !this.editState;
+      // this.$axios.post('other/momentList', {
+      // 	targetWxid: this.$store.state.currentPengyouquan,
+      // 	imei: util.getImei(),
+      // 	myWxid: util.getMyWxId()
+      // })
+      // .then(() => {
+      // 	debugger;
+      // })
+    },
+    getTags() {},
+    saveUserInf() {},
+    removeTag(index) {
+			var tag = this.tagData.splice(index, 1);
+			this.allTags = [...this.allTags, ...tag];
 		},
-		watch: {
-		},
-		components: {
-		},
-		methods: {
-			
-		},
-		created() {
-			// this.loadData();
+		checkTag(index){
+      var tag = this.allTags.splice(index, 1);
+			this.tagData = [...this.tagData, ...tag];
 		}
-	}
+  },
+  created() {
+    // this.loadData();
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-	.user_info {
-		display: flex;
-		flex-direction: column;
-		background: rgba(0, 0, 0, 0.6);
-		color: #fff;
-		min-height:100%;
-		padding-bottom: 12px;
+.user_info {
+  color: #fff;
+  padding-bottom: 12px;
+  height: 1px;
+  -webkit-box-flex: 1;
+  -ms-flex-positive: 1;
+  flex-grow: 1;
+  overflow: auto;
 
-		.user_info_item{
-			padding: 6px 12px;
+  .user_info_item {
+    padding: 6px 12px;
 
-			.user_info_title{
-				text-align: center;
-				font-size: 16px;
-				line-height: 2;
-			}
-			.user_info_wrap{
-				border-radius: 4px;
-				background-color: #333;
-				background-color:rgba(255,255,255,.1);
-				padding: 12px 0;
-				min-height: 80px;
+    .user_info_title {
+      text-align: center;
+      font-size: 16px;
+      line-height: 2;
+    }
+    .user_info_wrap {
+      border-radius: 4px;
+      background-color: rgba(0, 0, 0, 0.2);
+      padding: .6em 1em;
+      min-height: 80px;
+			font-size: .9em;
 
-				.user_info_wrap_li{
-					position: relative;
-					padding: .2em;
+      .user_info_wrap_li {
+        position: relative;
+        padding: 0.2em;
 
-					.user_info_wrap_sub{
-						color: #aaa;
-						width: 4em;
-						position: absolute;
-						left: 1em;
-						text-align: justify;
-        				text-align-last: justify;
-					}
+        .user_info_wrap_sub {
+          color: #aaa;
+          width: 4em;
+          position: absolute;
+          text-align: justify;
+          text-align-last: justify;
+        }
 
-					.user_info_wrap_text{
-						margin-left: 6.5em;
-						margin-right: .5em;
-						/*overflow: hidden;
-						text-overflow:ellipsis;
-						white-space: nowrap;*/
-						display: block;
-					}
+        .user_info_wrap_text {
+          margin-left: 5.5em;
+          margin-right: 0.5em;
+          display: block;
+          padding: 0.1em;
+          &.editor {
+            border: 1px solid rgba(255, 255, 255, 0.2);
+            background: rgba(255, 255, 255, 0.14);
+            border-radius: 4px;
+            padding: 0.04em 0.6em;
+            &:focus {
+              outline: none;
+            }
+          }
+        }
+      }
+      .user_info_wrap_tag {
+        display: inline-block;
+        border-radius: 4px;
+        border: 1px solid #666;
+        padding: 0 0.5em;
+        margin: 0.5em 0.3em;
+        line-height: 2;
+        position: relative;
+				font-size: .88em;
+        cursor: pointer;
+        .del {
+          width: 12px;
+          height: 12px;
+          display: block;
+          border-radius: 50%;
+          position: absolute;
+          right: -6px;
+          top: -6px;
+          background: #fff;
+          color: #000;
+          font-size: 13px;
+          text-align: center;
+          cursor: pointer;
+        }
+				&.no_sel{
+					color: #5297e8;
+					border-color: #5297e8;
 				}
-				.user_info_wrap_tag{
-					display: inline-block;
-					border-radius: 4px;
-					border: 1px solid #666;
-					padding: 0 .5em;
-					margin: .5em .3em;
-					line-height: 2;
-				}
+      }
+			.tag_tip{
+				margin: .1em;
 			}
-		}
-	}
+    }
+  }
+}
+
+.user_content {
+  height: 100%;
+  overflow: auto;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  display: flex;
+  flex-direction: column;
+  display: -webkit-box;
+  position: relative;
+  overflow: hidden;
+}
+.user_bottom {
+  padding: 10px;
+  text-align: center;
+}
 </style>
--- src/components/groupMessage.vue
+++ src/components/groupMessage.vue
@@ -1,178 +1,199 @@
 <template>
-<div style="top:50%;">
-    <group-left @returnSelectTag="returnSelectTag" :groupType="groupType" ref="groupLeft" @returnFriendList="returnFriendList"></group-left>
+  <div style="top:50%;">
+    <group-left
+      @returnSelectTag="returnSelectTag"
+      :groupType="groupType"
+      ref="groupLeft"
+      @returnFriendList="returnFriendList"
+    ></group-left>
     <group-right @retunContent="retunRightContent" @returnSendText="returnSendText"></group-right>
     <div class="clear"></div>
     <div style="padding:10px;">
-        <el-button type="primary" :loading="sendToLoad" @click="sendTo" :disabled="sendToFlag">发送</el-button>
+      <el-button type="primary" :loading="sendToLoad" @click="sendTo" :disabled="sendToFlag">发送</el-button>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
-import groupLeft from './groupLeft.vue'
-import groupRight from './groupRight.vue'
-import util from '@/util/util.js'
-export default{
-    data(){
-        return{
-            sendToFlag:true,
-            RightContentType:'' , //右边选中的dataType
-            RightContentSelectData:{} , //右边选中的Selectdata
-            LeftSelectTagData:{},
-            SendText:'',
-            sendToLoad:false,
-            friendList:[]
-        }
-    },
-    props:{
-        groupType:{
-            type:String,
-            default:''
-        }
-    },
-    mounted(){
-
-    },
-    components: {
-        groupLeft,
-        groupRight
-    },
-    methods:{
-        retunRightContent(type,item){
-            this.RightContentType = type;
-            this.RightContentSelectData = item;
-        },
-        returnFriendList(item){
-            this.friendList = item
-        },
-        returnSelectTag(item){
-            this.LeftSelectTagData = item
-        },
-        returnSendText(SendText){
-            this.SendText = SendText;
-        },
-        sendTo(){
-            let tagStr = '';
-            let momentType = '';
-            if(this.RightContentType == 1 ){
-                momentType = 4
-            }else if(this.RightContentType == 2 || this.RightContentType == 5){
-                momentType = 2
-            }else if(this.RightContentType ==3 ){
-                momentType = 1
-            }else if(this.RightContentType == 4){
-                momentType = 3
-            }
-            for(let i=0;i<this.LeftSelectTagData.length;i++){
-                if(i == this.LeftSelectTagData.length-1){
-                    tagStr = tagStr +this.LeftSelectTagData[i].labelID
-                }else{
-                    tagStr = tagStr +this.LeftSelectTagData[i].labelID +','
-                }
-            }
-            //设置sendToLoad为true避免多次点击
-            //这是发布朋友圈的 多了 this.$refs.groupLeft.openFlag 是否公开的值
-            if(this.groupType == 'groupFriend'){
-                this.$axios.post('/postMoment', 
-                    {
-                        "filePath": this.RightContentType != 3 ? this.RightContentSelectData.url : '',
-                        "imei": util.getImei(),
-                        "label": tagStr,
-                        "momentTitle":  this.RightContentType == 3 ? this.RightContentSelectData.title :this.SendText,
-                        "momentType":momentType,
-                        "myWxid": util.getMyWxId(),
-                        "publicMode": 2
-                    }
-                )
-                .then(data => {
-                    
-                })
-                .catch(() => {
-                    
-                });
-            }else{
-                 //这是群发消息
-                 //单个循环调用
-                 let fileType = '';
-                 let filePath = ''
-                 if(this.RightContentType == 2 || this.RightContentType == 5){
-                    fileType = 'IMAGE'
-                    filePath  = this.RightContentType.url
-                 }else if(this.RightContentType == 4){
-                    fileType = 'VIDEO'
-                    filePath  = this.RightContentType.url
-                 }else{
-                    fileType = ''
-                    filePath = ''
-                 }
-                for(let i=0;i<this.friendList.length;i++){
-                 let body = {}
-                 if(this.RightContentType == 2 || this.RightContentType == 5 || this.RightContentType == 4){
-                    body =    {
-                            "imei": util.getImei(),
-                            'createTime':new Date().getTime(),
-                            "fileType":fileType,
-                            "fileName":"",
-                            'filePath':filePath,
-                            'fileId':'',
-                            'targetWxid':this.friendList[0].targetWxid,
-                            "myWxid": util.getMyWxId()
-                        }
-                 }else{
-                        body =    {
-                            'createTime':new Date().getTime(),
-                            "imei": util.getImei(),
-                            "myWxid": util.getMyWxId(),
-                            "sendContent":this.RightContentSelectData.title,
-                            'targetWxid':this.friendList[0].targetWxid,
-                        }
-                    }
-                    this.$axios.post('/sendText', body)
-                    .then(data => {
-                    })
-                    .catch(() => {
-                        
-                    });
-                }
-            }
-        }
-    },
-    watch:{
-        RightContentSelectData:function(item){
-            if(item || this.SendText){
-                this.sendToFlag = false
-            }else{
-                this.sendToFlag = true
-            }
-        },
-        SendText:function(item){
-            if(this.RightContentSelectData && this.RightContentSelectData.title || item){
-                this.sendToFlag = false
-            }else{
-                this.sendToFlag = true
-            }
-        }
+import groupLeft from "./groupLeft.vue";
+import groupRight from "./groupRight.vue";
+import util from "@/util/util.js";
+export default {
+  data() {
+    return {
+      sendToFlag: true,
+      RightContentType: "", //右边选中的dataType
+      RightContentSelectData: {}, //右边选中的Selectdata
+      LeftSelectTagData: {},
+      SendText: "",
+      sendToLoad: false,
+      friendList: []
+    };
+  },
+  props: {
+    groupType: {
+      type: String,
+      default: ""
     }
-}
-
+  },
+  mounted() {},
+  components: {
+    groupLeft,
+    groupRight
+  },
+  methods: {
+    retunRightContent(type, item) {
+      this.RightContentType = type;
+      this.RightContentSelectData = item;
+    },
+    returnFriendList(item) {
+      this.friendList = item;
+    },
+    returnSelectTag(item) {
+      this.LeftSelectTagData = item;
+    },
+    returnSendText(SendText) {
+      this.SendText = SendText;
+    },
+    sendTo() {
+      let tagStr = "";
+      let momentType = "";
+      if (this.RightContentType == 1) {
+        momentType = 4;
+      } else if (this.RightContentType == 2 || this.RightContentType == 5) {
+        momentType = 2;
+      } else if (this.RightContentType == 3) {
+        momentType = 1;
+      } else if (this.RightContentType == 4) {
+        momentType = 3;
+      }
+      for (let i = 0; i < this.LeftSelectTagData.length; i++) {
+        if (i == this.LeftSelectTagData.length - 1) {
+          tagStr = tagStr + this.LeftSelectTagData[i].labelID;
+        } else {
+          tagStr = tagStr + this.LeftSelectTagData[i].labelID + ",";
+        }
+      }
+      //设置sendToLoad为true避免多次点击
+      //这是发布朋友圈的 多了 this.$refs.groupLeft.openFlag 是否公开的值
+      if (this.groupType == "groupFriend") {
+        this.$axios
+          .post("/postMoment", {
+            filePath:
+              this.RightContentType != 3 ? this.RightContentSelectData.url : "",
+            imei: util.getImei(),
+            label: tagStr,
+            momentTitle:
+              this.RightContentType == 3
+                ? this.RightContentSelectData.title
+                : this.SendText,
+            momentType: momentType,
+            myWxid: util.getMyWxId(),
+            publicMode: 2
+          })
+          .then(data => {})
+          .catch(() => {});
+      } else {
+        //这是群发消息
+        //单个循环调用
+        let fileType = "";
+        let filePath = "";
+        if (this.RightContentType == 2 || this.RightContentType == 5) {
+          fileType = "IMAGE";
+          filePath = this.RightContentType.url;
+        } else if (this.RightContentType == 4) {
+          fileType = "VIDEO";
+          filePath = this.RightContentType.url;
+        } else {
+          fileType = "";
+          filePath = "";
+        }
+        let params = thsi.msgContent(fileType, filePath);
+        this.sendGroupMsg(0, params)
+        
+      }
+    },
+    sendGroupMsg(i, params){
+        var $this = this;
+        let freads = this.friendList;
+        let targetWxid = freads[i].targetWxid;
+        let len = freads.length;
+        params.targetWxid = targetWxid;
+        this.$axios
+            .post("/sendText", params)
+            .then(data => {})
+            .finally(()=>{
+                if(i< len) {
+                    let t = Math.ceil(Math.random() * 1+1.5);
+                    setTimeout(() => {
+                        $this.sendGroupMsg(i++, params);
+                    }, t * 1000);
+                }
+            })
+            .catch(() => {});
+    },
+    msgContent(fileType, filePath){
+        let body = {
+            imei: util.getImei(),
+            createTime: new Date().getTime(),
+            myWxid: util.getMyWxId()
+        };
+        if (
+        this.RightContentType == 2 ||
+        this.RightContentType == 5 ||
+        this.RightContentType == 4
+        ) {
+            body = Object.assign({}, body, {
+                fileType: fileType,
+                fileName: "",
+                filePath: filePath,
+                fileId: "",
+            })
+        } else {
+            body = Object.assign({}, body, {
+                sendContent: this.RightContentSelectData.title,
+            })
+        }
+        return body;
+    }
+  },
+  watch: {
+    RightContentSelectData: function(item) {
+      if (item || this.SendText) {
+        this.sendToFlag = false;
+      } else {
+        this.sendToFlag = true;
+      }
+    },
+    SendText: function(item) {
+      if (
+        (this.RightContentSelectData && this.RightContentSelectData.title) ||
+        item
+      ) {
+        this.sendToFlag = false;
+      } else {
+        this.sendToFlag = true;
+      }
+    }
+  }
+};
 </script>
 <style lang="scss" scoped>
-    .clear{
-        clear: both;
-    }
-    button{
-        margin: 0 auto;
-        display: block;
-    }
-    .el-dialog.dialog{
-        top: 50% !important;
-    }
+.clear {
+  clear: both;
+}
+button {
+  margin: 0 auto;
+  display: block;
+}
+.el-dialog.dialog {
+  top: 50% !important;
+}
 </style>
 <style lang="scss">
-.groupMessage{
-    .el-dialog__body{
-        padding: 0px 20px !important;
-    }
+.groupMessage {
+  .el-dialog__body {
+    padding: 0px 20px !important;
+  }
 }
 </style>
