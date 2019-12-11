9861fecbf9fc0e20db69f3ef81661b5edf68f2f4
jinges <jinges2012@gmail.com>
2019-12-11 09:53:59 +0800

    去重

--- src/components/groupLeft.vue
+++ src/components/groupLeft.vue
@@ -140,7 +140,24 @@ import util from '@/util/util.js'
 
                     list = [...list,...item];
                 }
-                this.friendList = Array.from(new Set(list));
+                this.friendList = this.uniqueArray(list, 'wxid');
+            },
+            function uniqueArray(array, key){
+                var result = [array[0]];
+                for(var i = 1; i < array.length; i++){
+                    var item = array[i];
+                    var repeat = false;
+                    for (var j = 0; j < result.length; j++) {
+                    if (item[key] == result[j][key]) {
+                        repeat = true;
+                        break;
+                    }
+                    }
+                    if (!repeat) {
+                    result.push(item);
+                    }
+                }
+                return result;
             }
         },
         watch:{
